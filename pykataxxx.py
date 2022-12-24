da83f09b7bd03c64b0752cde8ca1a48c87fb419d
Hasler TEHOU <tehoumanoel96@gmail.com>
2022-12-24 19:41:45 +0100

    Python-ESA-PAPR-B1-XxTest-TP

--- pykataxxx.py
+++ pykataxxx.py
@@ -7,7 +7,7 @@
 # nbr_min = mdp.count(c.lower())
 # print("nombre de miniscule", nbr_min)
 # nbr_maj = mdp.count(c.upper())
-# print("nombre de magiscule", nbr_maj)
+# print("nombre de majuscule")
 #
 #
 # if taille_mdp <= 10 and nbr_min <= 1 and nbr_maj <= 1:
@@ -16,11 +16,11 @@
 #     print("pas ok")
 
 
-#taille_mdp = len(mdp)
+# taille_mdp = len(mdp)
 
 # if taille_mdp <= 10 and mdp.islower():
 #     print("Mot de passe correcte")
-# else:
+# else :
 #     print("Mot de passe incorrecte")
 
 # mdp = input("Entrer votre mot de passe : ")
@@ -46,35 +46,35 @@
 # print(nbr_min)
 # print(nbr_maj)
 
-# Déclaration des indexs itérant sur les valeurs respectifs magiscule, miniscule, chiffre
+# Déclaration des indexés itérant sur les valeurs respective majuscules, miniscule, chiffre
 # mag = 0
 # min = 0
 # chf = 0
 
-# #Déclaration des valeurs itérables dans des tableaaux de chaînes de caractères
-# magiscules = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-# miniscules = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-# chiffres = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+# #Déclaration des valeurs itérables dans des tableaux de chaînes de caractères majuscules = ["A", "B", "C", "D",
+# "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y",
+# "Z"] minuscules = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+# "t", "u", "v", "w", "x", "y", "z"] chiffres = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 #
-# elem_1 = magiscules[1]
+# elem_1 = majuscules[1]
 # print(elem_1)
-# elem_2 = magiscules[5]
+# elem_2 = majuscules[5]
 # print(elem_2)
 # elem_3 = chiffres[4]
-# print(elem_3)
+# print (elem_3)
 #
 # mot_de_passe = input("Entrer votre mot de passe : ")
-# taille_du_mot_de_passe = len(mot_de_passe)
+# taille_du_mot_de_passe = len (mot_de_passe)
 #
-# if (taille_du_mot_de_passe <= 10):
-#     for caractere in mot_de_passe:
-#         if caractere in miniscules:
+# if (taille_du_mot_de_passe <= 10) :
+#     for caractere in mot_de_passe :
+#         if caractere in minuscules :
 #             min += 1
 #             continue
-#         if caractere in magiscules:
+#         if caractere in majuscules :
 #             mag += 1
 #             continue
-#         if caractere in chiffres:
+#         if caractere in chiffres :
 #             chf += 1
 #             continue
 # if (min >= 1 and mag >= 1 and chf >= 1):
