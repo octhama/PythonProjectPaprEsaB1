15708a67a449c4756f8fccbe4ee6f6a115bb1ac0
Hasler TEHOU <tehoumanoel96@gmail.com>
2022-12-27 19:03:52 +0100

    Python-ESA-PAPR-B1-TestX-TP

--- MdpCheckFunc.py
+++ MdpCheckFunc.py
@@ -1,8 +1,13 @@
 def mdp_verif(x_mdp):
     """
 
-    :param x_mdp:
-    :return:
+    :param x_mdp: représentant tout mot de passes qui seraient traités par cette fonction
+    :return: une première instruction conditionnelle qui renvoie False si aucun mot de passe n'a été encodé.
+    La deuxième instruction conditionnelle retourne True si la taille du mot de passe est supérieure ou égale à 10
+    caractères. Une boucle itérative examine chaque caractère de la chaine de caractères encodé et faire le compte des
+    caractères nécessaire (minuscules, majuscules, chiffres, caractères spéciaux) pour le respect du format du mot
+    de passe. Enfin une instruction conditionnelle qui renvoie True si la condition suivante est respecté : minimum des
+    minuscules, majuscules, chiffres et caractères spéciaux.
     """
     if not x_mdp:
         return False
