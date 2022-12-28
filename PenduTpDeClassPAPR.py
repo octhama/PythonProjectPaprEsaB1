082f0ff07163831167a45880422e367d0d5a1a37
Hasler TEHOU <tehoumanoel96@gmail.com>
2022-12-28 02:11:49 +0100

    Python-ESA-PAPR-B1-TestX-TP

--- PenduTpDeClassPAPR.py
+++ PenduTpDeClassPAPR.py
@@ -5,23 +5,31 @@ mot = "oxymore"
 tentative = 7
 mot_masque = "_" * len(mot)
 lettres_proposees = ""
-print(mot_masque)
 
-while tentative > 0:
+while tentative > 0 and "_" in mot_masque:
+    # Print current state of the game
+    print(mot_masque)  # affiche le mot masqué à chaque tour de boucle
+    print(f"Tentatives restantes: {tentative}")
+    print(f"Lettres proposées: {lettres_proposees}")
+
     lettre = input("Indiquez votre lettre : ")
+
+    # Vérifier si la lettre a déjà été proposée
     while lettre in lettres_proposees:
         lettre = input("Indiquez une lettre non proposée : ")
-    lettres_proposees += lettre
-    if lettre not in mot:  # lettre non trouvée
-        tentative -= 1
-    else:
+        lettres_proposees += lettre  # Ajouter la lettre aux lettres déjà proposées
+        continue
+    # Vérifier si la lettre se trouve dans le mot à deviner
+    if lettre in mot:
         for i in range(len(mot)):
-            #  print(mot[i]) || auto incrementation dans les cases indexées contenant chaque caractère
             if mot[i] == lettre:
-                mot_masque = mot_masque[0:i] + lettre + mot_masque[i+1:]
-                print(mot_masque)
-if "_" not in mot_masque:
+                mot_masque = mot_masque[:i] + lettre + mot_masque[i + 1:]  # remplace-le "_" par la lettre trouvée
+    else:
+        tentative -= 1  # lettre non trouvée décrémente d'une tentative
+
+# Afficher le résultat final
+if "_" not in mot_masque:  # le mot a été trouvé
     exit("Félicitations ! Vous avez trouvé le mot")
-else:
+else:  # le mot n'a pas été trouvé
     print("Désolé, vous avez perdu. Le mot était : ", mot)
     exit()
